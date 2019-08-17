a827c7b1a1ddc9116df21ef683e99ca054359dcb
SomethingIsAlreadyTaken <spicemustflow1919@gmail.com>
2019-08-17 17:47:49 +0300

    added JS

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	'use strict';
+
+	$('.slider').slick({
+		prevArrow: '<button class="slider__arrow slider__arrow--prev">',
+		nextArrow: '<button class="slider__arrow slider__arrow--next">',
+		dots: true,
+		dotsClass: 'slider__dots'
+
+	});
+
+});
